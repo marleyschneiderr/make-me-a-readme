d1c8fa7d7bb7566a5f831c79a1c1b0b137b9fe1a
marleyschneiderr <marley33@uw.edu>
2023-03-02 20:45:59 -0800

    Adding first 5 questions associated with the ReadMe prompts

--- index.js
+++ index.js
@@ -5,7 +5,40 @@ const path = require("path"); // taking all of the const responses and putting t
 const generateMarkdown = require("./generateMarkdown"); // pulling what we create in generateMarkdown.js and putting it back in index.js 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Type the name of your project.",
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a detailed description of your project.",
+    },
+
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select the license that is applicable to your project.",
+        choices: ["None", "MIT", "BSD3", "APACHE2.0", "MPL2.0", "BSD2", "Boost1.0", "Creative Commons", "Other"],
+    },
+
+    {
+        type: "input",
+        name: "require",
+        message: "Provide a list of your project dependancies here:",
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What coding languages were used in the creation of your project?",
+    },
+
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
